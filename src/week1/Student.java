00b84efa3c5ba86256fbfc7b57de45acd892e744
Trần Thiên Sơn <tranthiensonuet@users.noreply.github.com>
2018-09-14 11:18:47 +0700

    oop update

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -8,12 +8,39 @@ public class Student {
     private String Group;
     private String Email;
     // TODO: khai báo các phương thức getter, setter cho Student
-
+    public String getName(){
+        return Name;
+    }
+    public void setName(String n){
+        n = Name;
+    }
+    public String getID(){
+        return ID;
+    }
+    public void setID(String n){
+        n = ID;
+    }
+    public String getGroup(){
+        return Group;
+    }
+    public void setGroup(String n){
+        n = Group;
+    }
+    public String getEmail(){
+        return Email;
+    }
+    public void setEmail(String n){
+        n = Email;
+    }
     /**
      * Constructor 1
      */
     Student() {
         // TODO:
+        Name = "Student";
+        ID = "000";
+        Group = "INT22041";
+        Email = "uet@vnu.edu.vn";
     }
 
     /**
@@ -24,6 +51,10 @@ public class Student {
      */
     Student(String n, String sid, String em) {
         // TODO:
+        Name = n;
+        ID = sid;
+        Email = em;
+        Group = "INT22041";
     }
 
     /**
@@ -32,10 +63,17 @@ public class Student {
      */
     Student(Student s) {
         // TODO:
+        Name = s.Name;
+        ID = s.ID;
+        Group = s.Group;
+        Email = s.Email;
     }
 
-    String getInfo() {
+    public void getInfo() {
         // TODO:
-        return null; // xóa dòng này sau khi cài đặt
+        System.out.println("Name: " + getName());
+        System.out.println("ID: " + getID());
+        System.out.println("Group: " + getGroup());
+        System.out.println("Email: " + getEmail());
     }
 }
--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -1,11 +1,16 @@
 package week1;
 
+
 public class StudentManagement {
 
     // TODO: khai báo thuộc tính students là array chứa các đối tượng thuộc lớp Student (max. 100)
-
+    public static Student students[] = new Student[100];
     public boolean sameGroup(Student s1, Student s2) {
         // TODO:
+        String group1 = s1.getGroup();
+        String group2 = s2.getGroup();
+        if (group1.equals(group2))
+            return true;
         return false; // xóa dòng này sau khi cài đặt
     }
 
@@ -19,5 +24,13 @@ public class StudentManagement {
 
     public static void main(String[] args) {
         // TODO:
+        students[0] = new Student();
+        students[0].setName("Trần Thiên Sơn");
+        students[0].setID("17021006");
+        students[0].setGroup("INT2204");
+        students[0].setEmail("17021006@vnu.edu.vn");
+        System.out.println(students[0].getName());
+        students[0].getInfo();
+
     }
 }
