47ce6b16fba8b3e06cb858dac717e3257003ee87
Trần Thiên Sơn <43133165+tranthiensonuet@users.noreply.github.com>
2018-09-16 16:30:05 +0700

    Update StudentManagement.java

--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -3,7 +3,7 @@ package week1;
 
 
 public class StudentManagement {
-
+    public static int limit = 5;
     // TODO: khai báo thuộc tính students là array chứa các đối tượng thuộc lớp Student (max. 100)
     public static Student[] students = new Student[100];
 
@@ -17,48 +17,79 @@ public class StudentManagement {
     }
     public static void studentsByGroup() {
         // TODO:
-    }
+        boolean kt[] = new boolean[100];
+        for (int i = 0; i < limit; i++)
+            kt[i] = true;
+        String str;
+        for (int i = 0; i < limit - 1; i++)
+        {
+            if (students[i].getGroup() != null)
+            {
+                if (kt[i])
+                {
+                    str = students[i].getGroup();
+                    System.out.println("Sinh vien thuoc lop " + str + " la: ");
+                    for (int j = i; j < limit; j++)
+                    {
+                        if (kt[j] && str.equals(students[j].getGroup()) && students[j] != null)
+                        {
+                            kt[j] = false;
+                            System.out.println(students[j].getInfo());
+                        }
 
+                    }
+                }
+            }
+        }
+
+    }
     public static void removeStudent(String id) {
         // TODO:
+        int i = 0;
+        while (i < limit)
+        {
+            if (students[i].getID().equals(id))
+            {
+                for (int j = i; j < limit - 1; j++) {
+                    students[j] = students[j + 1];
+                }
+                limit--;
+            }
+            else i++;
+        }
+        for (int j = 0; j < limit; j++)
+            System.out.println(students[j].getInfo());
     }
 
     public static void main(String[] args) {
         // TODO:
-        StudentManagement st = new StudentManagement();
         //sinh vien 1
-        st.students[0] = new Student();
-        st.students[0].setName("Trần Thiên Sơn");
-        st.students[0].setID("17021006");
-        st.students[0].setGroup("INT22041");
-        st.students[0].setEmail("17021006@vnu.edu.vn");
-        System.out.println(st.students[0].getInfo());
+        students[0] = new Student();
+        students[0].setName("Trần Thiên Sơn");
+        students[0].setID("17021006");
+        students[0].setGroup("INT22041");
+        students[0].setEmail("17021006@vnu.edu.vn");
+        System.out.println(students[0].getInfo());
         //sinh vien 2
-        st.students[1] = new Student();
-        System.out.println(st.students[1].getInfo());
+        students[1] = new Student();
+        System.out.println(students[1].getInfo());
         //sinh vien 3
-        st.students[2] = new Student("Nguyen Van A", "17020001", "17021001@vnu.edu.vn");
-        System.out.println(st.students[2].getInfo());
+        students[2] = new Student("Nguyen Van A", "17020001", "17021001@vnu.edu.vn");
+        System.out.println(students[2].getInfo());
         //sinh vien 4
-        st.students[3] = new Student(st.students[0]);
-        System.out.println(st.students[3].getInfo());
-        st.students[0].getInfo();
-        //sinh vien 2
-        st.students[1] = new Student();
-        st.students[1].getInfo();
-        //sinh vien 3
-        st.students[2] = new Student("Nguyen Van A", "17020001", "17021001@vnu.edu.vn");
-        st.students[2].getInfo();
-        //sinh vien 4
-        st.students[3] = new Student(st.students[0]);
-        st.students[3].getInfo();
-        st.students[1].setGroup("INT22042");
-        students[2].setGroup("INT22041");
-        st.students[3].setGroup("INT22042");
-        if (st.sameGroup(st.students[1], st.students[2]))
-            System.out.println("Cung lop");
-        else
-            System.out.printf("Khac lop");
+        students[3] = new Student(students[0]);
+        System.out.println(students[3].getInfo());
+        //sinh vien 5
+        students[4] = new Student("Pham Thi B", "17020002", "17020002@gmail.com");
+        System.out.println(students[4].getInfo());
+        students[1].setGroup("INT22041");
+        students[2].setGroup("INT22042");
+        students[3].setGroup("INT22041");
+        students[4].setGroup("INT22042");
+        System.out.println(sameGroup(students[1], students[2]));
+        studentsByGroup();
+        System.out.println("Danh sach xoa la: ");
+        removeStudent("17020001");
 
 
     }
