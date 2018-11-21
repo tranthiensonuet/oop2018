ef1f663465fc8d7d24a3b79406b929a023b1aae2
SonThien <tranthienson1999@gmail.com>
2018-11-21 23:11:46 +0700

    update

--- src/week11/Task1.java
+++ src/week11/Task1.java
@@ -1,4 +1,15 @@
 package week11;
 
+import java.util.Arrays;
+
 public class Task1 {
+    static <T> void Sort(T[] a){
+        Arrays.sort(a);
+    }
+
+    public static void main(String[] args) {
+        String a[] = {"son", "thien", "huy", "hung", "nam", "cua"};
+        Sort(a);
+        System.out.println(Arrays.toString(a));
+    }
 }
--- src/week11/Task2.java
+++ src/week11/Task2.java
@@ -1,4 +1,16 @@
 package week11;
 
+import java.util.Arrays;
+
 public class Task2 {
+    static <T> T Sort (T a[]){
+        Arrays.sort(a);
+        return a[a.length - 1];
+    }
+
+    public static void main(String[] args) {
+        String a[] = {"son", "thien", "huy", "hung", "nam", "cua"};
+        String max = Sort(a);
+        System.out.println(max);
+    }
 }
